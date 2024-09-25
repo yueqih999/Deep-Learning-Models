582da517e8d49b2e28775fa6fb34c2637db9758e
yueqih999 <yueqih@hotmail.com>
2024-09-25 02:23:53 -0400

    3

--- RNN/LSTM/train.py
+++ RNN/LSTM/train.py
@@ -136,7 +136,7 @@ if __name__ == "__main__":
     parser.add_argument('--dropout', type=float, default=0.5, help='Dropout probability')
     parser.add_argument('--num_layers', type=int, default=2, help='Number of LSTM layers')
     parser.add_argument('--decay_epochs', type=int, default=7, help='Number of epochs to divide lr')
-    parser.add_argument('--decay_co', type=int, default=2, help='Divide Learning Rate')
+    parser.add_argument('--decay_co', type=float, default=2, help='Divide Learning Rate')
 
     args = parser.parse_args()
 
